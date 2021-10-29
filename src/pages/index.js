ac2aef9412b2af26631616b5fcdcf2a1e01eca1c
effierichman <55716328+effierichman@users.noreply.github.com>
2021-10-29 14:14:07 -0400

    Add Favicon and Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,26 @@ import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
+import Head from "next/head";
 
 const Home = () => {
   return (
-    <Layout>
-      <Section grid style={{ overflowY: "scroll" }}>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      {/* <Timeline /> */}
-      {/* <Acomplishments /> */}
-    </Layout>
+    <div>
+      <Head>
+        <title>Effie's Portfolio</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Layout>
+        <Section grid style={{ overflowY: "scroll" }}>
+          <Hero />
+          <BgAnimation />
+        </Section>
+        <Projects />
+        <Technologies />
+        {/* <Timeline /> */}
+        {/* <Acomplishments /> */}
+      </Layout>
+    </div>
   );
 };
 //THS IS WHERE IT HAPPENS
